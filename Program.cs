9eeef4f3394558905a4549dec4316020c372fc48
Marcelo Torterolo <marcelo.torterolo@middlesoft.com.uy>
2019-11-14 14:56:09 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         const string _alphanum = "0123456789!@#$%^&*ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+         int _size = _alphanum.Length;
+
+         //password length
+         int length = 8;
+
+         //srand(time(0));
+         Random rnd = new Random((int)DateTime.Now.Ticks);
+
+         for (int i = 0; i < length; i++)
+         {
+            Console.Write(_alphanum[rnd.Next() % _size]);
+         }
+         Console.ReadKey();
+      }
+   }
+}
