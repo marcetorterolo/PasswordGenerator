385afc537688015403a4c298c4bf4b73ee6185f7
Marcelo Torterolo <marcetorterolo@gmail.com>
2019-11-14 15:12:36 -0300

    Se mejora codigo

--- Program.cs
+++ Program.cs
@@ -1,26 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace PasswordGenerator
 {
    class Program
    {
+      static string generatePswd(int length)
+      {
+         const string validChars = "0123456789!@#$%^&*ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+         StringBuilder res = new StringBuilder();
+
+         Random rnd = new Random((int)DateTime.Now.Ticks);
+         while (0 < length--)
+         {
+            res.Append(validChars[rnd.Next() % validChars.Length]);
+         }
+         return res.ToString();
+      }
+
       static void Main(string[] args)
       {
-         const string _alphanum = "0123456789!@#$%^&*ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-         int _size = _alphanum.Length;
-
          //password length
          int length = 8;
 
-         Random rnd = new Random((int)DateTime.Now.Ticks);
-         for (int i = 0; i < length; i++)
-         {
-            Console.Write(_alphanum[rnd.Next() % _size]);
-         }
+         string pass = generatePswd(length);
+         Console.Write(pass);
          Console.ReadKey();
       }
    }
